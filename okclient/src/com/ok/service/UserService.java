304033e3ef95c04dacc615dc961cdfcf69f7d341
Hiki <ainip9324@qq.com>
2025-06-13 00:19:39 +0800

    登录界面初步设计和部分内容完成

---
+++ okclient/src/com/ok/service/UserService.java
@@ -0,0 +1,30 @@
+package com.ok.service;
+
+import com.ok.po.User;
+
+import java.io.*;
+import java.net.Socket;
+
+public class UserService {
+
+    public boolean login(User user){
+        try{
+            //和本机建立连接通信
+            Socket socket = new Socket("127.0.0.1",8888);
+
+            //获取输出流，向外输出内容
+            OutputStream outputStream = socket.getOutputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+            objectOutputStream.writeObject(user);
+            //通过socket连接的输入流，读入服务器的返回信息
+            InputStream inputStream = socket.getInputStream();
+            ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+}
---
+++ okclient/src/com/ok/view/LoginView.java
@@ -0,0 +1,81 @@
+package com.ok.view;
+
+import com.ok.po.User;
+import com.ok.service.UserService;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class LoginView extends JFrame {
+
+    public static void main(String[] args) {
+        LoginView loginView = new LoginView();
+        loginView.createFrame();
+    }
+
+    JPanel northJPanel = null;
+    JLabel photooJLabel = null;
+
+    JLabel userNameJLabel = null;
+    JTextField userNameJTextField = null;
+    JLabel pwdJLabel = null;
+    JPasswordField pwdJPasswordField = null;
+    JPanel centerJPanel = null;
+
+    JButton loginJButton = null;
+    JButton registerJButton = null;
+    JPanel southJPanel = null;
+
+    public void createFrame(){
+        ImageIcon imageIcon = new ImageIcon(LoginView.class.getClassLoader().getResource("com/ok/images/logo.png"));
+        imageIcon.setImage(imageIcon.getImage().getScaledInstance(100,100,Image.SCALE_DEFAULT));
+        photooJLabel = new JLabel("",imageIcon,JLabel.CENTER);
+        northJPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        northJPanel.add(photooJLabel);
+        this.add(northJPanel,BorderLayout.NORTH);
+
+        userNameJLabel = new JLabel("账号：");
+        userNameJTextField = new JTextField(21);
+        userNameJTextField.setFont(new Font(Font.DIALOG,Font.PLAIN,18));
+        pwdJLabel = new JLabel("密码：");
+        pwdJPasswordField = new JPasswordField(21);
+        pwdJPasswordField.setFont(new Font(Font.DIALOG,Font.PLAIN,18));
+        centerJPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        centerJPanel.add(userNameJLabel);
+        centerJPanel.add(userNameJTextField);
+        centerJPanel.add(pwdJLabel);
+        centerJPanel.add(pwdJPasswordField);
+        this.add(centerJPanel,BorderLayout.CENTER);
+
+        loginJButton = new JButton("登录");
+        loginJButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                User user = new User();
+                user.setUserName(userNameJTextField.getText());
+                user.setPwd(new String(pwdJPasswordField.getPassword()));
+
+                UserService userService = new UserService();
+                if(userService.login(user)){
+                    LoginView.this.dispose();
+                }else{
+                    JOptionPane.showMessageDialog(LoginView.this,"账号或密码错误","提示",JOptionPane.WARNING_MESSAGE);
+                }
+                super.mouseClicked(e);
+            }
+        });
+        registerJButton = new JButton("注册");
+        southJPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        southJPanel.add(loginJButton);
+        southJPanel.add(registerJButton);
+        this.add(southJPanel,BorderLayout.SOUTH);
+
+        this.setTitle("OkChat登录");
+        this.setBounds(505,300,400,350);
+        this.setVisible(true);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
